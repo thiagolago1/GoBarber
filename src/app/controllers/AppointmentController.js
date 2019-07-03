2d518ad2d13eb670060ed3d576179d6d736013f8
Thiago Lago <thiagolagoo2@gmail.com>
2019-07-03 17:08:49 -0300

    :building_construction: [ADD] Appointment Controller

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,40 @@
+import * as Yup from 'yup';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    // Yup Lib controla o schema; faz validações
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Failed! ' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    // Checar se o provider_id é um provider
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You Can Only Create Appointments with Providers! ' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +20,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
