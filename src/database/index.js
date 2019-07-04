ad26442809ed25923ae265db86acade4fd2d14df
Thiago Lago <thiagolagoo2@gmail.com>
2019-07-04 19:11:43 -0300

    :heavy_plus_sign: [ADD] Mongoose Connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 // Arquivo que realiza a conexão com o DB e carregar as Models
 
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -21,6 +22,14 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models)); // Condição para executar só se o método existir
+  }
+
+  // Configuração de conexão com o MongoDB para dados não estruturados
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
   }
 }
 
