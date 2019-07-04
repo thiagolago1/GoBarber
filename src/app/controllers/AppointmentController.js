7191b2b5949340a0b36b0f1323dd7366cd05d84a
Thiago Lago <thiagolagoo2@gmail.com>
2019-07-04 18:36:53 -0300

    :zap: [ADD] Pagination

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,12 +6,16 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       // Listando agendamentos do usuário por data
       // Selecionando os atributos mostrando o relacionamento com o provider
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
