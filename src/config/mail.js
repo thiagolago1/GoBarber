8e6ff10d4bed453cc56014afe5994d03f674551d
Thiago Lago <thiagolagoo2@gmail.com>
2019-07-07 01:01:17 -0300

    :heavy_plus_sign: [ADD] Nodemailer and send emails to cancel appointments

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+// Ambiente e credenciais de Dev
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'a108e6d5e576d8',
+    pass: '6ebe6a3130ea60',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
