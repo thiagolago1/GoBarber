be3f62a1e3a3db9e03676301b1a5ad2daae61da8
Thiago Lago <thiagolagoo2@gmail.com>
2019-07-08 18:00:33 -0300

    :heavy_plus_sign: [ADD] Bee-queue depedency, :whale: Docker Redis and :sparkles: configuration of Queues

---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,34 @@
+import pt from 'date-fns/locale/pt';
+import { format, parseISO } from 'date-fns';
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  get key() {
+    return 'CancellationMail';
+  }
+
+  async handle({ data }) {
+    const { appointment } = data;
+
+    console.log('A Fila foi executada!');
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new CancellationMail();
---
+++ src/lib/Queue.js
@@ -0,0 +1,44 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+// Para cada job se cria uma fila e dentro da fila se armazena o 'Bee'
+// 'bee' é a instância que conecta com o redis ( que armazena e recupera valores do DB)
+// handle processa a fila, recebe as variáveis e processa
+// processQueue processa o job em tempo real
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  // botando um novo job na fila em background
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();
