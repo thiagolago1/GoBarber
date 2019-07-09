6422970ab44e2a8023006e95fee185ff3e72b697
Thiago Lago <thiagolagoo2@gmail.com>
2019-07-09 17:19:42 -0300

    :lock: [ADD] Adding exception handling with Sentry

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 // Express - Framework Back-end para apps web do NodeJS
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+import 'express-async-errors';
 import routes from './routes';
 
 import './database';
@@ -11,11 +15,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json()); // Habilitando a app para receber json
     this.server.use(
       '/files',
@@ -25,6 +33,16 @@ class App {
 
   routes() {
     this.server.use(routes); // Importando as rotas
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // Método para captar os errors
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 // Exportando o server
